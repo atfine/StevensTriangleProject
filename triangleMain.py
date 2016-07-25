26383dbff1755c148b429009a60ed5f794e41335
atfine <alexander.fine@rockwellcollins.com>
2016-07-25 08:59:12 -0500

    first draft
    needs finished

--- triangleMain.py
+++ triangleMain.py
@@ -1,3 +1,28 @@
 # Main file for Triangle project
 
-print "Triangles!"
+isocelesType = "Isoceles"
+scaleneType = "Scalene"
+equilateralType = "Equilateral"
+
+def main:
+  print "Parameter a = 3"
+  a = 5
+  print "Parameter b = 4"
+  b = 4
+  print "Parameter c = 5"
+  c = 5
+  
+  triangleType = classifyTriangle(a,b,c)
+  
+  
+  
+def classifyTriangle(a,b,c):
+  
+  validTriangle = False
+  
+  if (a == b & b == c):
+    theType = equilateralType
+  elif (a == b | a == c | b == c)
+    theType = isocelesType
+  else:
+  	theType = scaleneType

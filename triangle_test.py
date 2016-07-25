e0b15950f3018e120a087fcca3def9f25501ab22
Michael Carman <michael.carman@rockwellcollins.com>
2016-07-25 10:17:48 -0500

    Split code/tests
    Moved tests into separate module. Run via "python -m unittest
    triangle_test"

---
+++ triangle_test.py
@@ -0,0 +1,31 @@
+import unittest
+import triangleMain
+from triangleMain import classifyTriangle
+
+equilateral = 'Equilateral'
+isoceles    = 'Isoceles'
+scalene     = 'Scalene'
+right       = 'Right Scalene'
+invalid     = 'Invalid'
+
+class TestClassifyTriangle(unittest.TestCase):
+  def test_equilateral(self):
+    self.assertEqual(classifyTriangle(3,3,3), equilateral)
+
+  def test_isoceles(self):
+    self.assertEqual(classifyTriangle(3,3,4), isoceles)
+
+  def test_scalene(self):
+    self.assertEqual(classifyTriangle(1,3,5), scalene)
+
+  def test_right(self):
+    self.assertEqual(classifyTriangle(3,4,5), right)
+
+  def test_nonnumber(self):
+    self.assertEqual(classifyTriangle(5,'b',7), invalid)
+
+  def test_negative_length(self):
+    self.assertEqual(classifyTriangle(1,2,-3), invalid)
+
+if __name__ == '__main__':
+  unittest.main()
